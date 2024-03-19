966765626adad601ed07ffbffd885ab0f4a40eba
Benson7878 <bensonnyanchieo78@gmail.com>
2024-03-19 15:01:56 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 1000) +1;
+const num3 =32;
+const num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(2,7,10, 12,16,20);
